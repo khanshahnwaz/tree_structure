3fba415356c0db8ce73a7630fbc286cdc3d52baf
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2023-08-26 20:57:13 +0530

    Done

--- src/Components/Child.jsx
+++ src/Components/Child.jsx
@@ -1,45 +1,120 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import { FiChevronDown } from "react-icons/fi";
 
-const Child = ({index, name, data, children, onEditData, onAddChild,onNameChange }) => {
+const Child = ({
+  index,
+  name,
+  data,
+  children,
+  onEditData,
+  onAddChild,
+  onNameChange,
+}) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [newName, setNewName] = useState(name);
-  const [childData, setChildData] = useState("This is initial");
-  const handleEditData = (newData) => {
-    onEditData(newData);
+  const [textData, setTextData] = useState(data);
+  const [editData, setEditData] = useState(false);
+  const [editName, setEditName] = useState(false);
+  useEffect(() => {
+    setTextData(data);
+  }, [data]);
+  const handleEditData = (event) => {
+    // event.stopPropagation();
+    console.log("calling edit for ", name);
+    onEditData(name, textData, event);
+    setEditData(false);
+    // e.stopPropagation();    // console.log(e.target)
   };
-const handleNameChange=()=>{
-  console.log("old name",name, " and new name ",newName)
-       onNameChange(name,newName)
-}
-  const handleAddChild = (parentName,ind) => {
-    console.log("clicked name is",parentName)
-    onAddChild(parentName,ind);
+  const handleNameChange = () => {
+    console.log(
+      "calling for ",
+      name,
+      "old name",
+      name,
+      " and new name ",
+      newName
+    );
+    onNameChange(name, newName);
+    setEditName(false);
+  };
+  const handleAddChild = (parentName, ind) => {
+    console.log("clicked name is", parentName);
+    onAddChild(parentName, ind);
   };
 
   return (
     <div className="border-4 border-blue-400 mx-10 my-10">
-    <div className="flex w-full justify-between  bg-blue-400 px-2 py-4">
-      <div>
-        <div className="flex gap-x-2">
-    <button className="p-3 bg-gray-300" onClick={() => setIsCollapsed(!isCollapsed)}>
-      {isCollapsed ? "▶️" : "🔽"} 
-    </button><input name='name' readOnly={true} value={newName} className="outline-none bg-inherit m-auto text-lg font-semibold" onClick={(e)=>e.target.readOnly=false} onMouseLeave={(e)=>e.target.readOnly=true} onChange={(e)=>[setNewName(e.target.value),handleNameChange()]}/></div></div>
-    <button onClick={(e)=>handleAddChild(name,index)} className="px-3 py-1 bg-gray-300">Add Child</button>
-    </div>
+      <div className="flex w-full justify-between  bg-blue-400 px-2 py-4">
+        <div>
+          <div className="flex gap-x-2">
+            <button
+              className="p-3 bg-gray-300"
+              onClick={() => setIsCollapsed(!isCollapsed)}
+            >
+              {isCollapsed ? <FiChevronRight /> : <FiChevronDown />}
+            </button>
+            {!editName ? (
+              <b className="my-auto" onClick={() => setEditName(true)}>
+                {newName}
+              </b>
+            ) : (
+              <input
+                value={newName}
+                className=" m-auto text-lg font-semibold"
+                onKeyDown={(e) => {
+                  console.log("clicked key is", e.key);
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    handleNameChange();
+                  }
+                }}
+                onChange={(e) => [setNewName(e.target.value)]}
+              />
+            )}
+          </div>
+        </div>
+        <button
+          onClick={(e) => handleAddChild(name, index)}
+          className="px-3 py-1 bg-gray-300"
+        >
+          Add Child
+        </button>
+      </div>
       {!isCollapsed && (
         <div>
-          <p>
-            Data:
-            <input
-              type="text"
-              value={data}
-              onChange={(e) => {
-                setChildData(e.target.value);
-                handleEditData(e.target.value);
-              }}
-            />
-          </p>
-        
+          {textData ? (
+            editData ? (
+              <div className="text-left p-3">
+                {" "}
+                Data:
+                <input
+                  className="px-5 py-2 border-2 border-blue-300"
+                  type="text"
+                  value={textData}
+                  onChange={(e) => setTextData(e.target.value)}
+                  // onBlur={handleEditData}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      // console.log("key is",e)
+                      e.preventDefault();
+                      e.stopPropagation();
+
+                      handleEditData(e);
+                    }
+                  }}
+                />
+              </div>
+            ) : (
+              <div className="text-left p-3" onClick={() => setEditData(true)}>
+                Data:{" "}
+                <span className="px-5 py-2 border-2 border-gray-300">
+                  {textData}
+                </span>
+              </div>
+            )
+          ) : null}
           {children?.map((child, index) => (
             <Child
               key={index}
@@ -47,7 +122,7 @@ const handleNameChange=()=>{
               name={child.name}
               data={child.data}
               children={child.children}
-              onEditData={(newData) => handleEditData(newData)}
+              onEditData={handleEditData}
               onAddChild={handleAddChild}
               onNameChange={handleNameChange}
             />
--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,196 +1,116 @@
-import React, { useEffect, useState } from 'react';
-import Child from './Child';
-function App() {
-  const[exportData,setExportData]=useState();
-  const[showData,setShowData]=useState(false);
-  const [rootChildren, setRootChildren] = useState([{name:'Root',data:'this is me'}]);
+import React, { useEffect, useState } from "react";
+import Child from "./Child";
+function LandingPage() {
+  const [exportData, setExportData] = useState();
+  const [showData, setShowData] = useState(false);
+  const [rootChildren, setRootChildren] = useState([
+    { name: "Root", data: "this is me" },
+  ]);
 
-  const handleAddChild = (Name,index) => {
-    console.log("Clicked parent is ",Name , "and index is",index)
+  const handleAddChild = (Name, index) => {
+    // console.log("Clicked parent is ", Name, "and index is", index);
     const updatedChildren = [...rootChildren];
-    
-    const checkObject=(updatedChildren)=>{
-        console.log("in checking",updatedChildren)
-        for(let i in updatedChildren){
-           
-            if(updatedChildren[i].name==Name){
-              if(!updatedChildren[i].children){
-          updatedChildren[i].children=[]
-              const {data,...rest}=updatedChildren[i];
-             
-              }
-    let children=updatedChildren[i].children;
-    updatedChildren[i].children.push({ name: `${Name} Child ${children?children.length+1:1}`, data: `${Name} Data` });
-              console.log("parent is found ",updatedChildren[i])
-              return;
-            }
-              
-             else if(updatedChildren[i].children){
-              checkObject(updatedChildren[i].children)
-             }
-           console.log("childrens are ",updatedChildren[i].name)
+    const checkObject = async (updatedChildren) => {
+      // console.log("in checking", updatedChildren);
+      for (let i in updatedChildren) {
+        if (updatedChildren[i].name == Name) {
+          if (!updatedChildren[i].children) {
+            updatedChildren[i].children = [];
+            const { data, ...rest } = updatedChildren[i];
+            updatedChildren[i] = rest;
+            setRootChildren(updatedChildren);
+            console.log("family is", updatedChildren);
+          }
+          let children = updatedChildren[i].children;
+          updatedChildren[i].children.push({
+            name: `${Name} Child ${children ? children.length + 1 : 1}`,
+            data: `${Name} Data`,
+          });
+          // console.log("parent is found ", updatedChildren[i]);
+          return;
+        } else if (updatedChildren[i].children) {
+          checkObject(updatedChildren[i].children);
         }
-    }
+        // console.log("childrens are ", updatedChildren[i].name);
+      }
+    };
 
     checkObject(updatedChildren);
     setRootChildren(updatedChildren);
-    console.log(updatedChildren)
+    // console.log(updatedChildren);
   };
 
-  const handleNameChange=(name,newName)=>{
+  const handleNameChange = (name, newName) => {
     const updatedChildren = [...rootChildren];
-  const checkObject=(updatedChildren)=>{
-    for(let i in updatedChildren){
-           
-      if(updatedChildren[i].name==name){
-         updatedChildren[i].name=newName;
-        return;
+    const checkObject = (updatedChildren) => {
+      for (let i in updatedChildren) {
+        if (updatedChildren[i].name == name) {
+          updatedChildren[i].name = newName;
+          return;
+        } else if (updatedChildren[i].children) {
+          checkObject(updatedChildren[i].children);
+        }
       }
-        
-       else if(updatedChildren[i].children){
-        checkObject(updatedChildren[i].children)
-       }
-      
-  }
-}
-checkObject(updatedChildren)
-setRootChildren(updatedChildren)
-  }
-
-  const handleEditData = (index, newData) => {
-    const updatedChildren = [...rootChildren];
-    updatedChildren[index].data = newData;
+    };
+    checkObject(updatedChildren);
     setRootChildren(updatedChildren);
   };
-useEffect(()=>{
-  setExportData(JSON.stringify(rootChildren[0]))
-},[rootChildren])
+
+  const handleEditData = (tagName, newData) => {
+    console.log("editing dat for ", tagName, " data is ", newData);
+    const checkObject = (rootChildren) => {
+      for (let i in rootChildren) {
+        console.log("current name is", rootChildren[i].name);
+        if (rootChildren[i].name == tagName) {
+          rootChildren[i].data = newData;
+          console.log("edited ", rootChildren[i]);
+          return;
+        } else if (rootChildren[i].children) {
+          checkObject(rootChildren[i].children);
+        }
+      }
+    };
+    checkObject(rootChildren);
+    setRootChildren(rootChildren);
+    setExportData(JSON.stringify(rootChildren[0]));
+    console.log("after data edition ", rootChildren);
+  };
+  useEffect(() => {
+    console.log("i am changed");
+    setExportData(JSON.stringify(rootChildren[0]));
+  }, [rootChildren]);
   return (
     <div>
-      
-      
       {rootChildren.map((child, index) => (
         <div key={index}>
           {/* <button onClick={() => handleAddChild(index)}>Add Child</button> */}
           <Child
             index={index}
-            name= {child.name}
+            name={child.name}
             data={child.data}
             children={child.children}
-            onEditData={(newData) => handleEditData(index, newData)}
+            onEditData={handleEditData}
             onAddChild={handleAddChild}
             onNameChange={handleNameChange}
             // ({index,parentName}) => handleAddChild(index,parentName)
           />
         </div>
       ))}
-<div className='mx-10'>
-<button className='px-3 py-2 bg-gray-300 float-left rounded shadow-md' onClick={()=>setShowData(!showData)}>Export data</button>
-{showData?<div>{exportData}</div>:null}
-</div>
+      <div className="mx-10 grid gap-y-2">
+        <button
+          className="px-3 py-2 bg-gray-300  rounded shadow-md text-left w-max"
+          onClick={() => setShowData(!showData)}
+        >
+          Export data
+        </button>
+        {showData ? (
+          <div className="p-2 border-2 border-blue-300 font-bold">
+            {exportData}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
 
-export default App;
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-
-// function Child({ name, data, children, onEditData, onAddChild }) {
-//   const [isCollapsed, setIsCollapsed] = useState(false);
-//   const [childName, setChildName] = useState('');
-//   const [childData, setChildData] = useState('');
-
-//   const handleEditData = (newData) => {
-//     onEditData(newData);
-//   };
-
-//   const handleAddChild = () => {
-//     onAddChild({ name: 'New Child', data: 'Data', children: [] });
-//   };
-
-//   return (
-//     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-//       <h3 onClick={() => setIsCollapsed(!isCollapsed)}>
-//         {isCollapsed ? '▶️' : '🔽'} {name}
-//       </h3>
-//       {!isCollapsed && (
-//         <div>
-//           <p>
-//             Data: 
-//             <input
-//               type="text"
-//               value={data}
-//               onChange={(e) => {
-//                 setChildData(e.target.value);
-//                 handleEditData(e.target.value);
-//               }}
-//             />
-//           </p>
-//           <button onClick={handleAddChild}>Add Child</button>
-//           {children.map((child, index) => (
-//             <Child
-//               key={index}
-//               name={child.name}
-//               data={child.data}
-//               children={child.children}
-//               onEditData={(newData) => handleEditData(newData)}
-//               onAddChild={handleAddChild}
-//             />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// function App() {
-//   const [rootChildren, setRootChildren] = useState([]);
-
-//   const handleAddChild = (index) => {
-//     const updatedChildren = [...rootChildren];
-//         updatedChildren[index].children.push({ name: 'New Child', data: 'Data', children: [] });
-//         setRootChildren(updatedChildren);
-//   };
-
-//   const handleEditData = (index, newData) => {
-//     const updatedChildren = [...rootChildren];
-//     updatedChildren[index].data = newData;
-//     setRootChildren(updatedChildren);
-//   };
-
-//   const handleExport = () => {
-//     const exportData = JSON.stringify(rootChildren, ['name', 'children', 'data'], 2);
-//     console.log(exportData);
-//     // You can do whatever you want with the exportData, such as displaying it in a modal or downloading it.
-//   };
-
-//   return (
-//     <div>
-//       <h2>Parent Component</h2>
-//       <button onClick={() => handleAddChild({ name: 'Child', data: '', children: [] })}>
-//         Add Child
-//       </button>
-//       <button onClick={handleExport}>Export</button>
-//       {rootChildren.map((child, index) => (
-//         <Child
-//           key={index}
-//           name={child.name}
-//           data={child.data}
-//           children={child.children}
-//           onEditData={(newData) => handleEditData(index, newData)}
-//           onAddChild={()=>handleAddChild(index)}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default App;
+export default LandingPage;
